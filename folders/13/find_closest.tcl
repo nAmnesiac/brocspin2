bf30b2a6b32e85871358bdfe358cd765ef8dd7af
nAmnesiac <allen.m.chen07@gmail.com>
2025-12-07 02:38:19 -0500

    modified tcl script to output in copy pasteable format

--- folders/13/find_closest.tcl
+++ folders/13/find_closest.tcl
@@ -4,10 +4,9 @@ set other_atoms [atomselect top "not resid 50"]
 
 foreach idx $res50_indices {
     set atom1 [atomselect top "index $idx"]
-    set atom1_name [$atom1 get name]
-    set atom1_resid [$atom1 get resid]
     set atom1_resname [$atom1 get resname]
     set atom1_coords [lindex [$atom1 get {x y z}] 0]
+    set atom1_id [expr $idx + 1]
     
     set distances {}
     foreach other_idx [$other_atoms get index] {
@@ -25,14 +24,14 @@ foreach idx $res50_indices {
         set dist [lindex $pair 0]
         set other_idx [lindex $pair 1]
         set atom2 [atomselect top "index $other_idx"]
-        set atom2_name [$atom2 get name]
-        set atom2_resid [$atom2 get resid]
         set atom2_resname [$atom2 get resname]
-        puts [format "%s:%s (index %d) - %s:%s (index %d): %.3f Å" $atom1_resname $atom1_name $idx $atom2_resname $atom2_name $other_idx $dist]
+        set atom2_id [expr $other_idx + 1]
+        set dist_nm [expr $dist / 10.0]
+        puts [format "%d %d %s %s %.3f" $atom1_id $atom2_id $atom1_resname $atom2_resname $dist_nm]
         $atom2 delete
     }
     $atom1 delete
 }
 
 $res50_atoms delete
-$other_atoms delete 
+$other_atoms delete
